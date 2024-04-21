53e3275dd24c23c0cd45da5e575e8259a1a70e92
kkras <kkrastev@live.com>
2024-04-21 23:26:43 +0300

    Added editorconfig

--- verse.Mesh.Net.CheckoutService/obj/Debug/net8.0/verse.Mesh.Net.CheckoutService.AssemblyInfo.cs
+++ verse.Mesh.Net.CheckoutService/obj/Debug/net8.0/verse.Mesh.Net.CheckoutService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("verse.Mesh.Net.CheckoutService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0d509800853181e3a575ddff52ed876b01e7b3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c96f1ea1ffa73777ffbf7b88ca5265c9ba9ccab7")]
 [assembly: System.Reflection.AssemblyProductAttribute("verse.Mesh.Net.CheckoutService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("verse.Mesh.Net.CheckoutService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
